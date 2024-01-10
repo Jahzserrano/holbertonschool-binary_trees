49bf2f93dbc782cb47dbd4bc7a9fc635c81db255
ispdls <is.pdls90@gmail.com>
2024-01-10 14:39:22 -0400

    node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: new node
+ * Return: new node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+     
+    binary_tree_t *new_nd = malloc(sizeof(binary_tree_t));
+
+    if (new_nd == NULL) {
+        return NULL;  
+    }
+    
+    new_nd->n = value;
+    new_nd->parent = parent;
+    new_nd->left = NULL;
+    new_nd->right = NULL;
+
+    return new_nd;
+
+}
